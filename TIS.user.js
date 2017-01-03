5b29a6581c7cbc05cd32f823643319e266e7bca5
lkomatl <dst.kb@yandex.ru>
2017-01-03 22:49:10 +0500

    Update TIS.user.js

--- TIS.user.js
+++ TIS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TIS
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       kombat
 // @domain      https://pernatsk.ru/*
