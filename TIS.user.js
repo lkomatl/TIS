e07335adfacd1e99afbb2bd7b66e85ca62f91998
lkomatl <dst.kb@yandex.ru>
2017-01-07 15:35:20 +0500

    Update TIS.user.js

--- TIS.user.js
+++ TIS.user.js
@@ -1,8 +1,6 @@
 // ==UserScript==
 // @name         TIS
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
+// @version      0.2.1
 // @author       kombat
 // @domain      https://pernatsk.ru/*
 // @namespace   https://pernatsk.ru/*
@@ -11,15 +9,13 @@
 // ==/UserScript==
 
 (function() {
-var zvukosnov = true;  //основное звуковое сопровождение, по умолчанию
-var zvukSled = true;
-
+var zvukosnov = "1";  //основное звуковое сопровождение, по умолчанию
+var zvukSled = "1";
 var addr = location.pathname; // алиас относительного пути
 var vol = 1; //громкость по умолчанию
 var link1 = "https://p.nnm.su/assets/frog.mp3"; //звук оповещения по умолчанию
 var link2 = "https://wav-library.net/sounds/0-0-1-11084-20"; //звук оповещения для новогодних инвентов по умолчанию
 var poltaim = 5; // время до оповещения по умолчанию
-
 
 //проверка запуск функции для стандартного опопвещения
     if (localStorage["pernatskTIS.zvukosnov"] == "1" ){
@@ -36,12 +32,11 @@ tm = tm * 60000;
  ts = ts * 1000;
 var to = th + tm  + ts;
 setTimeout(function() {audio.play()}, to);}}
-    
-    
-   if (localStorage["pernatskTIS.zvukSled"] == "1" ){    
+
+   if (localStorage["pernatskTIS.zvukSled"] == "1" ){
     //санколёт
 if(document.getElementById('mini-content-timer-snowhill') !== null){
-var sled=document.getElementById('mini-content-timer-snowhill'); 
+var sled=document.getElementById('mini-content-timer-snowhill');
 var tSled=sled.innerHTML; //достаём и коверкаем таймер
 var thSled = tSled.substring(0, tSled.length - 6);
 var tmSled = tSled.substring(3, tSled.length - 3);
@@ -56,15 +51,14 @@ var toSled = thSled + tmSled  + tsSled;
     setTimeout(function() {audioSled.play()}, toSled);
 }}
 
-   if (localStorage["pernatskTIS.zvukSled"] == "1" ){    
-    //санколёт
+   if (localStorage["pernatskTIS.zvukSled"] == "1" ){
+    //снегокос
 if(document.getElementById('mini-content-timer-snowcleaner') !== null){
-var snowcleaner=document.getElementById('mini-content-timer-snowcleaner'); 
+var snowcleaner=document.getElementById('mini-content-timer-snowcleaner');
 var tsnowcleaner=snowcleaner.innerHTML; //достаём и коверкаем таймер
 var thsnowcleaner = tsnowcleaner.substring(0, tsnowcleaner.length - 6);
 var tmsnowcleaner = tsnowcleaner.substring(3, tsnowcleaner.length - 3);
 var tssnowcleaner = tsnowcleaner.substring(6);
- 
     if (tssnowcleaner < poltaim ){tssnowcleaner = 0;}
     else {tssnowcleaner = tssnowcleaner - poltaim;}
         //таймер
@@ -72,12 +66,28 @@ thsnowcleaner = thsnowcleaner * 3600000;
 tmsnowcleaner = tmsnowcleaner * 60000;
  tssnowcleaner = tssnowcleaner * 1000;
 var tosnowcleaner = thsnowcleaner + tmsnowcleaner  + tssnowcleaner;
-  
-    setTimeout(function() {audioSled.play()}, tosnowcleaner);
-}}    
-    
-    
-    
+   setTimeout(function() {audioSled.play()}, tosnowcleaner);
+}}
+
+   if (localStorage["pernatskTIS.zvukSled"] == "1" ){
+    //снежный городок
+if(document.getElementById('mini-content-timer-snowtown') !== null){
+var snowtown=document.getElementById('mini-content-timer-snowtown');
+var tsnowtown=snowtown.innerHTML; //достаём и коверкаем таймер
+
+//var thsnowtown = thsnowtown.substring(0, tsnowsnowtown.length - 6);
+var tmsnowtown = tsnowtown.substring(0, tsnowtown.length - 3);
+var tssnowtown = tsnowtown.substring(3);
+    if (tssnowtown < poltaim ){tssnowtown = 0;}
+    else {tssnowtown = tssnowtown - poltaim;}
+        //таймер
+//thsnowcleaner = thsnowcleaner * 3600000;
+tmsnowtown= tmsnowtown * 60000;
+ tssnowtown = tssnowtown * 1000;
+var tosnowtown =  tmsnowtown  + tssnowtown;
+   setTimeout(function() {audioSled.play()}, tosnowtown);
+}}
+
 // Загружаем настройки
 	if (supportsLocalStorage()) {
         //громкость
@@ -87,7 +97,7 @@ var tosnowcleaner = thsnowcleaner + tmsnowcleaner  + tssnowcleaner;
         //ссылка на звук(основная)
 		if (typeof localStorage["pernatskTIS.link1"] == "undefined") {
 			localStorage["pernatskTIS.link1"] = link1;
-		} 
+		}
         //пользовательское время(до пилика)
 		if (typeof localStorage["pernatskTIS.poltaim"] == "undefined") {
 			localStorage["pernatskTIS.poltaim"] = poltaim;
@@ -110,20 +120,20 @@ var tosnowcleaner = thsnowcleaner + tmsnowcleaner  + tssnowcleaner;
 			return false;
 		}
 	}
-    
-//создаём звук (основной)    
+
+//создаём звук (основной)
     var   audio = new Audio();
-    audio.volume= localStorage["pernatskTIS.vol"] / 10;// настриваем громкость 
+    audio.volume= localStorage["pernatskTIS.vol"] / 10;// настриваем громкость
     audio.src = localStorage["pernatskTIS.link1"];
-    
-// проба звука для инвентов    
+
+// проба звука для инвентов
     var   audioSled = new Audio();
-    audioSled.volume= localStorage["pernatskTIS.vol"] / 10;// настриваем громкость 
-    audioSled.src = localStorage["pernatskTIS.link2"];    
+    audioSled.volume= localStorage["pernatskTIS.vol"] / 10;// настриваем громкость
+    audioSled.src = localStorage["pernatskTIS.link2"];
 // настройки
 	if (addr == '/nest/bird/settings') {
 		// форма с настройками юзерскрипта
-		var settingsFormThemes = 
+		var settingsFormThemes =
 		'<div id="settingsFormThemes">'+
 		'	<div class="stat-ct">Настрокий TIS</div>'+
 		'	<div class="set-3">'+
@@ -136,7 +146,7 @@ var tosnowcleaner = thsnowcleaner + tmsnowcleaner  + tssnowcleaner;
 		'		<div class="set-action">'+
 		'			<b>Врмя оповещения</b>:'+
 		'			<input  type="text" id="poltaim" name="poltaim" class="flea-searching-input">'+
-        '		сек. '+   
+        '		сек. '+
 		'		</div>'+
 		'		</div>'+
 		'		<div class="set-action">'+
@@ -172,51 +182,49 @@ var tosnowcleaner = thsnowcleaner + tmsnowcleaner  + tssnowcleaner;
             localStorage["pernatskTIS.link1"] = link1;
             localStorage["pernatskTIS.poltaim"] = poltaim;
             //сохраниние чекбокса для основного
-            localStorage["pernatskTIS.zvukosnov"] = zvukosnov;   
+            localStorage["pernatskTIS.zvukosnov"] = zvukosnov;
                     if (zvukosnov=== true){
             localStorage["pernatskTIS.zvukosnov"] = 1;}
              else if (zvukosnov == "true"){
              localStorage["pernatskTIS.zvukosnov"] = 2;}
-             else  {localStorage["pernatskTIS.zvukosnov"] = 0;}    
+             else  {localStorage["pernatskTIS.zvukosnov"] = 0;}
             //соханение чекбокса для зимниз инвентов
-            localStorage["pernatskTIS.zvukSled"] = zvukSled;   
+            localStorage["pernatskTIS.zvukSled"] = zvukSled;
                     if (zvukSled=== true){
             localStorage["pernatskTIS.zvukSled"] = 1;}
              else if (zvukSled == "true"){
              localStorage["pernatskTIS.zvukSled"] = 2;}
-             else  {localStorage["pernatskTIS.zvukSled"] = 0;}            
+             else  {localStorage["pernatskTIS.zvukSled"] = 0;}
             console.log(vol);
             console.log(link1);
             console.log(poltaim);
             console.log(zvukosnov);
-			
+
             location.reload();
 		});
 //подстановка значений в инпут
-        document.getElementById('zvukosnov').checked =  +localStorage["pernatskTIS.zvukosnov"];  
+        document.getElementById('zvukosnov').checked =  +localStorage["pernatskTIS.zvukosnov"];
         document.getElementById('link1').setAttribute('value', localStorage["pernatskTIS.link1"]);
         document.getElementById('link2').setAttribute('value', localStorage["pernatskTIS.link2"]);
         document.getElementById('vol').setAttribute('value', localStorage["pernatskTIS.vol"]);
         document.getElementById('poltaim').setAttribute('value', localStorage["pernatskTIS.poltaim"]);
-        document.getElementById('zvukSled').checked =  +localStorage["pernatskTIS.zvukSled"];  
-       
-//подстановка значения звука првее бегунка	
+        document.getElementById('zvukSled').checked =  +localStorage["pernatskTIS.zvukSled"];
+
+//подстановка значения звука првее бегунка
   var input = document.getElementById('vol');
   document.getElementById('result').innerHTML = localStorage["pernatskTIS.vol"];
   input.oninput = function() {
     document.getElementById('result').innerHTML = input.value;
-  }; 
-	
+  };
    //подстановка значений в чекбоксы
   var checkbox = document.getElementById('zvukosnov');
     checkbox.onchange = function() {
-        zvukosnov =  checkbox.checked ;    
-    };     
+        zvukosnov =  checkbox.checked ;
+    };
   var checkboxSled = document.getElementById('zvukSled');
     checkboxSled.onchange = function() {
-        zvukSled =  checkboxSled.checked ;    
-    }; 
+        zvukSled =  checkboxSled.checked ;
+    };
     }
- 
 
 })();
